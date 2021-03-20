a937a5ed11614d054792aa70d413457f68119582
tonicanada <toni.cm@gmail.com>
2021-03-20 14:49:08 -0300

    modifies response

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  req.send("it is working!");
+  res.send("it is working!");
 });
 app.post("/signin", handleSignin(db, bcrypt));
 
