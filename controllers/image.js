1638552bdd1b2a3e62deca19fb98045052843bf3
tonicanada <toni.cm@gmail.com>
2021-03-20 14:24:33 -0300

    initial commit

---
+++ controllers/image.js
@@ -0,0 +1,24 @@
+import Clarifai from "clarifai";
+
+export const app = new Clarifai.App({
+  apiKey: "ef05de48ac8b4e83b7a35c5446e3791a",
+});
+
+export const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("unable to work with API"));
+  };
+
+export const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => res.json(entries[0]))
+    .catch((err) => res.status(400).json("unable to get entries"));
+};
---
+++ server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import cors from "cors";
+import knex from "knex";
+import handleRegister from "./controllers/register.js";
+import handleSignin from "./controllers/signin.js";
+import handleProfileGet from "./controllers/profile.js";
+import { handleImage, handleApiCall } from "./controllers/image.js";
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "acm",
+    password: "gandhi_149",
+    database: "smart-brain",
+  },
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/signin", handleSignin(db, bcrypt));
+
+app.post("/register", (req, res) => {
+  handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  handleProfileGet(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  handleImage(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  handleApiCall(req, res);
+});
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
